5a93768b8f0f2f28aba03062caf29877dff6b1a7
Fahad MB <fahadmbasheer@gmail.com>
2026-08-27 14:57:20 +0530

    feat:add Video Access

--- Frontend/src/socket.ts
+++ Frontend/src/socket.ts
@@ -2,14 +2,14 @@ const socket = new WebSocket("ws://localhost:5000")
 
 socket.onopen = () => {
   console.log("WebSocket connected")
-};
+}
 
 socket.onclose = () => {
   console.log("WebSocket disconnected")
-};
+}
 
 socket.onerror = (error) => {
   console.error("WebSocket error:", error)
-};
+}
 
 export default socket
