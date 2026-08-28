24f0247a10f279573d90c1ab1b9de0432cb34e29
Fahad MB <fahadmbasheer@gmail.com>
2026-08-28 18:55:35 +0530

    Update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,7 @@
 import app from "./app";
-import { createWebSocketServer } from "./websocket/websocketServer";
 
 const PORT = 5000;
 
-const server = app.listen(
-  PORT,
-  () => {
-    console.log(
-      `Server running on port ${PORT}`
-    );
-  }
-);
-
-createWebSocketServer(server);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
